901b8c4f447a6da795e2c971d8e16dfe171c49a6
ValentinaPortillo <138265214+ValentinaPortillo@users.noreply.github.com>
2024-11-11 10:47:14 -0300

    Add files via upload

---
+++ ProgII/ProyectoProgII/src/Main.java
@@ -0,0 +1,35 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.text.ParseException;
+
+
+public class Main{
+    public static void main(String[] args){
+
+        Connection connection = null;
+        try {
+            connection = conexion.Obtenerconexion();
+            System.out.println("Conexion realizada");
+
+            //laburen
+            Persona.TPersona persona = new Persona.TPersona(); //objeto utilizado para accedeer a los metodos/registros de la clase
+            persona.crearTabla();
+            persona.crearObjeto(); //creo el objeto desde el main
+
+            // persona1.FuncionImc(id1)
+
+
+        } catch (SQLException e) {
+            System.out.println("Error:" + e.getMessage());
+        }
+        finally {
+            try {
+                if (connection != null )  {connection.close();
+                    System.out.println("la conexion se finalizo");}
+            } catch (SQLException e){
+                System.out.println("Error:" + e.getMessage());
+            }
+        }
+    }
+}
